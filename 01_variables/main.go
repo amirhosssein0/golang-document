8c51f49bf62030e407df7ce7e0c718cc2b9d6fc9
amirhosssein <real.amirhosssein0@gmail.com>
2025-10-23 19:25:38 +0330

    variables

---
+++ 01_variables/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	// // Declaring variables
+	var a int
+	var b string
+	fmt.Println(a)
+	fmt.Println(b)
+
+	// Initializing Variables
+	a = 10 //assignment
+	fmt.Println(a)
+
+	// Creating and initializing variables
+	var c = 10
+	var d = "amirhossein"
+	fmt.Println(c)
+	fmt.Println(d)
+
+	// Declaring multiple variables
+	var e, f int
+	fmt.Println(e, f)
+	e = 10
+	f = 100
+	fmt.Println(e, f)
+	// Creating and initializing multiple variables
+	var g, h = 10, 100
+	fmt.Println(g, h)
+
+	// Variable Declaration Block
+	var (
+		i = 10
+		j = "amirhossein"
+		k = 1000
+	)
+
+	fmt.Println(i, j, k)
+
+	//Short variable declaration
+	var l int
+	var m int
+	l, m, n := 10, 20, 30
+	fmt.Println(l, m, n)
+}
